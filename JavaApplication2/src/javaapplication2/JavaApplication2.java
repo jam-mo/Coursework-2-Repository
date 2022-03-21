08344d3ae0caaa051dc437b0b9da6da8ee69d2fc
w1790671 <w1790671@e-g25-pc-23.intranet.wmin.ac.uk>
2022-03-21 16:48:33 +0000

    Add debug

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -17,6 +17,7 @@ public class JavaApplication2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello, goodbye");
+        System.out.println("HII< LOVE RICE >");
     }
     
 }
